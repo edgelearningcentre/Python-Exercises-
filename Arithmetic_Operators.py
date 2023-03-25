4aa5255db2d85d46ce2dcb1d1ae3bab71471422b
Edge Learning Centre <85828473+edgelearningcentre@users.noreply.github.com>
2023-03-25 15:59:00 +0530

    Create Arithmetic_Operators.py

---
+++ Arithmetic_Operators.py
@@ -0,0 +1,13 @@
+"""
+Title     : Arithmetic Operators
+Language  : Python
+Author    : Neeraj Julka
+Created   : 25 March, 2023
+Problem   : https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+"""
+if __name__ == "__main__":
+    a = int(input())
+    b = int(input())
+    print(a + b)
+    print(a - b)
+    print(a * b)
